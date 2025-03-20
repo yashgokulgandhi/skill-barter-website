27da3bed0a2c9ff07eacf9b4f11f7dcf3b53a355
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-03-20 18:48:41 +0530

    deployement changes 3

--- Skill-Barter-frontend/vite.config.js
+++ Skill-Barter-frontend/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'stompjs': 'stompjs/lib/stomp.js' // Force browser-compatible STOMP
-    }
-  }
+ 
 })
